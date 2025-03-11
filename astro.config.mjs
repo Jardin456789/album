57a7c978ffa7e9f7ec32634510eb0418eea09218
David Uch <v.david.uch@gmail.com>
2025-03-11 13:29:36 +0100

    Correction de l'import Vercel déprécié

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,9 @@ export default defineConfig({
 
   integrations: [react()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
